03a0457ba2d252a1bc01e9a00eede87680765018
J2b2590 <J2b2590@gmail.com>
2020-10-19 17:54:47 -0500

    added new,create, and strong params to games controller, add  initializer to the config application.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,26 @@
 class GamesController < ApplicationController
+
+
+    def new
+        @game = Game.new
+    end
+
+    def create
+
+        @court = Court.all
+        @host = User.all
+
+        @game = Game.new(game_params)
+        @game.save 
+
+        
+    end
+
+
+
+    private
+
+    def game_params
+        params.require(:game).permit(:host_id, :court_id, :date, :time , :player_count)
+    end
 end
