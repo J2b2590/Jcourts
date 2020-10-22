65acbe573d3afa4d7c2b701b91d69b4f8f566d41
jsingle345 <jsingle345@gmail.com>
2020-10-21 20:17:24 -0500

    Finished validation for adding players to game

--- app/models/player_game.rb
+++ app/models/player_game.rb
@@ -2,5 +2,6 @@ class PlayerGame < ApplicationRecord
     belongs_to :player, :class_name => "User"
     belongs_to :game 
 
-    #validates_uniqueness_of :player_id, :scope => :name
+    validates :player_id, presence: true, :uniqueness => 
+    { :scope => :player_id, message: " is already in the game" }
 end
